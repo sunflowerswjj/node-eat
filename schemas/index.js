a4c5eb29e8cf03a6ce4196d7faddf6903e078945
zhaohd <zhaohaodang@gmail.com>
2017-08-11 16:54:48 +0800

    init

--- schemas/index.js
+++ schemas/index.js
@@ -7,19 +7,22 @@ mongoose.connect(config.mongodb)
 db.on('error', console.error.bind(console, 'connection error:'))
 db.on('open', function() {
   var userSchema = Schema({
-    nickname: { type: String, required: true },
+    nickname: { first: String, last: String },
     date: { type: Date, default: Date.now },
     password: { type: String },
     avatar: { type: String, default: 'https://ws1.sinaimg.cn/large/b9691c2dgy1ficl10isxaj20jr0jr40m.jpg' },
     gender: { type: String }
   })
+  userSchema.virtual('fullname').get(function() {
+    return this.nickname.first + ' ' + this.nickname.last
+  })
   userSchema.methods.speak = function() {
-    console.log('my name is: ' + this.name)
+    console.log('my name is: ' + this.fullname)
   }
   var User = mongoose.model('User', userSchema)
-  var zhaohd = new User({ name: 'zhaohd' })
+  var zhaohd = new User({ nickname: { first: 'zhao', last: 'hd' }, password: '1111' })
   zhaohd.save(function(err, zhaohd) {
     if (err) return console.error(err)
-    zhaohd.speak()
+    console.log(zhaohd)
   })
 })
